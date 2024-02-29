3fb8c645958aa12422abf9cb6e9a0adcbdff5c33
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-02-29 07:49:59 -0300

    first commit

---
+++ src/main/java/io/github/eduardoconceicao90/TesteIntegracao.java
@@ -0,0 +1,9 @@
+package io.github.eduardoconceicao90;
+
+public class TesteIntegracao {
+
+    public static void main(String[] args) {
+        var user = "Gere 5 produtos";
+        var system = "Gere produtos ficticios para um ecommerce apenas com os nomes dos produtos solicitatos pelo usuario";
+    }
+}
