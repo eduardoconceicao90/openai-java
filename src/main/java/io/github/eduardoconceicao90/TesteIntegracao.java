26e1439a587a07da7e24a534a055977a02e92017
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-02-29 08:27:58 -0300

    Realizando configurações iniciais para integração com a API

--- src/main/java/io/github/eduardoconceicao90/TesteIntegracao.java
+++ src/main/java/io/github/eduardoconceicao90/TesteIntegracao.java
@@ -1,9 +1,28 @@
 package io.github.eduardoconceicao90;
 
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.util.Arrays;
+
 public class TesteIntegracao {
 
     public static void main(String[] args) {
         var user = "Gere 5 produtos";
         var system = "Gere produtos ficticios para um ecommerce apenas com os nomes dos produtos solicitatos pelo usuario";
+
+        var service = new OpenAiService("your_token");
+        var chatCompletionRequest = ChatCompletionRequest
+                                    .builder()
+                                    .model("gpt-3.5-turbo")
+                                    .messages(Arrays.asList(
+                                            new ChatMessage(ChatMessageRole.USER.value(), user),
+                                            new ChatMessage(ChatMessageRole.SYSTEM.value(), system)
+                                    ))
+                                    .build();
+
+        service.createChatCompletion(chatCompletionRequest).getChoices().forEach(System.out::println);
     }
 }
