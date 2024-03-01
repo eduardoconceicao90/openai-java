81a965b31e376b3c81a761e7e8e2df3184892dbd
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-03-01 17:01:23 -0300

    Escolha dinâmica do modelo

--- src/main/java/io/github/eduardoconceicao90/TesteIntegracao.java
+++ src/main/java/io/github/eduardoconceicao90/TesteIntegracao.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 public class TesteIntegracao {
 
     public static void main(String[] args) {
+
         var user = "Gere 5 produtos";
         var system = "Gere produtos ficticios para um ecommerce apenas com os nomes dos produtos solicitatos pelo usuario";
 
